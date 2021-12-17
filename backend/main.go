e1e3044f2044987c051e64943e3de988adc17a53
SemmiDev <sammidev4@gmail.com>
2021-12-17 19:52:21 +0700

    refactor main.go

--- backend/main.go
+++ backend/main.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"embed"
+	"log"
 
 	"github.com/Xanvial/todo-app-go/backend/api"
 	"github.com/Xanvial/todo-app-go/backend/datastore"
-	"github.com/Xanvial/todo-app-go/model"
+	"github.com/Xanvial/todo-app-go/backend/util"
 )
 
-// embed the static web to binary file
 //go:embed webstatic/*
 var htmlData embed.FS
 
 func main() {
-	// define the data store type & create the server
-	s := api.NewServer(datastore.Map, htmlData)
-	// run the server on port in config
-	s.Start(model.AppPort)
+	config, err := util.LoadConfig("..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	server := api.NewServer(config, htmlData, datastore.Postgre)
+	server.Start(config.AppPort)
 }
